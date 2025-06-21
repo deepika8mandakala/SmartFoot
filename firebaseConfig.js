71e428c76ea51fac54a692bee059c014d592d96e
sathvika <sathvika3005@gmail.com>
2025-06-21 19:38:41 +0530

    made changes
    anges
    made changes

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCsN5MfK7DCeGiidFtcioriN3n6srA5yF0",
+  authDomain: "smartroute-dec54.firebaseapp.com",
+  projectId: "smartroute-dec54",
+  storageBucket: "smartroute-dec54.firebasestorage.app",
+  messagingSenderId: "737757522088",
+  appId: "1:737757522088:web:b5fff35603a614a1b5fa9d",
+  measurementId: "G-W5D0Z6SBW0"
+};
